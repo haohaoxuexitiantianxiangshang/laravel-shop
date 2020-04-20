ab327ebeb6fdac9a7a448b08b08cdc9361812e32
huangbingquan <hbq@yiban.cn>
2020-04-20 18:06:35 +0800

    新分支

--- resources/views/business/products/index.blade.php
+++ resources/views/business/products/index.blade.php
@@ -16,26 +16,26 @@
                                                 <img src="{{ $product->image_url }}" alt="">
                                             </a>
                                         </div>
-                                        <div class="price"><b>￥</b>{{ $product->price }}</div>
                                         <div class="title">
-                                            <a href="{{ route('products.show', ['product' => $product->id]) }}">{{ $product->title }}</a>
+                                            <span>{{ $product->title }}</span>
                                         </div>
+                                        @foreach($product->skus as $sku)
+                                            <div class="row">
+                                                <div class="price col-8"><b>￥</b>{{  $sku->price }}</div>
+                                                <div class="sku-btn col-2"
+                                                     data-price="{{ $sku->price }}"
+                                                     data-stock="{{ $sku->stock }}"
+                                                     data-toggle="tooltip"
+                                                     title="{{ $sku->description }}"
+                                                     data-placement="bottom">
+                                                    <input type="radio" name="skus-{{$product->id}}" autocomplete="off"
+                                                           value="{{ $sku->id }}"> {{ $sku->title }}
+                                                </div>
+                                            </div>
+                                        @endforeach
                                     </div>
-                                    @foreach($product->skus as $sku)
-                                        <label
-                                            class="btn sku-btn"
-                                            data-price="{{ $sku->price }}"
-                                            data-stock="{{ $sku->stock }}"
-                                            data-toggle="tooltip"
-                                            title="{{ $sku->description }}"
-                                            data-placement="bottom">
-                                            <input type="radio" name="skus" autocomplete="off"
-                                                   value="{{ $sku->id }}"> {{ $sku->title }}
-                                        </label>
-                                    @endforeach
-
-
-                                    <button class="btn btn-warning btn-lg btn-block btn-add-to-cart" type="submit">购买
+                                    <button class="btn btn-warning btn-lg btn-block btn-add-to-cart"
+                                            value="{{$product->id}}" type="submit">购买
                                     </button>
                                 </div>
                             </div>
@@ -64,10 +64,12 @@
 
         // 加入购物车按钮点击事件
         $('.btn-add-to-cart').click(function () {
+            skus_name = 'skus-' + $(this).val()
+            console.log('input[name=' + skus_name + ']');
             // 请求加入购物车接口
             axios.post('{{ route('cart.add') }}', {
-                sku_id: $('label.active input[name=skus]').val(),
-                amount: $('.cart_amount input').val(),
+                sku_id: $('input[name=' + skus_name + ']').val(),
+                amount: 1,
             })
                 .then(function () { // 请求成功执行此回调
                     swal('加入购物车成功', '', 'success')
