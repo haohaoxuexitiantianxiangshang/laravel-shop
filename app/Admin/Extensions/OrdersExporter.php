fede8710870e18e722c5ccc563054cebbaabf0ff
huangbingquan <hbq@yiban.cn>
2020-05-09 17:04:59 +0800

    订单的导出excel

--- app/Admin/Extensions/OrdersExporter.php
+++ app/Admin/Extensions/OrdersExporter.php
@@ -4,38 +4,46 @@ namespace App\Admin\Extensions;
 
 use Encore\Admin\Grid\Exporters\ExcelExporter;
 use Maatwebsite\Excel\Concerns\WithMapping;
-
 class OrdersExporter extends ExcelExporter implements WithMapping
 {
 
     protected $fileName = '订单.xlsx';
 
-    protected $columns = [
-        'no' => '订单流水号',
-        'user_id' => '买家',
-        'area' => '地区',
-        'address' => '地址',
-        'address.phone' => '电话',
-        'created_at' => '下单时间',
-        'total_amount' => '总金额',
-        'ship_status' => '物流',
-        'remark' => '备注',
-        'id' => '商品信息',
-    ];
+    public function headings(): array
+    {
+        return [
+            '订单流水号',
+            '买家',
+            '地区',
+            '地址',
+            '电话',
+            '下单时间',
+            '总金额',
+            '备注',
+            '物流',
+            '涨芝士',
+            '老酸奶',
+            '简醇',
+            '白小纯',
+            '鲜活牛乳',
+            '优果酪',
+            '白小纯燕麦',
+            '芝芝好莓',
+            '芝芝好芒'
+        ];
+    }
 
 
     public function map($order): array
     {
-
-
         $goosItems = $order->items()->with(['product', 'productSku'])->get();
-        $goosItemsString = '';
-        foreach ($goosItems as $item) {
-            $item->productName = $item->product->title;
-            $item->productSkuName = $item->productSku->title;
-            $goosItemsString .= "商品-" . $item->productName . "--" . $item->productSkuName . "--数量--" . $item->amount . "\r\n";
-        }
+        $goosItemsObject = (object)[];
 
+        foreach ($goosItems as $item) {
+            $productName = $item->product->title;
+            $productSkuName = $item->productSku->title;
+            $goosItemsObject->$productName = $item->amount;
+        }
         return [
             $order->no,
             data_get($order, 'user.name'),
@@ -44,8 +52,17 @@ class OrdersExporter extends ExcelExporter implements WithMapping
             $order->address['phone'],
             $order->created_at,
             $order->total_amount,
+            $order->remark,
             $order->ship_status,
-            $order->item = $goosItemsString
+            data_get($goosItemsObject, '涨芝士'),
+            data_get($goosItemsObject, '老酸奶'),
+            data_get($goosItemsObject, '简醇'),
+            data_get($goosItemsObject, '白小纯'),
+            data_get($goosItemsObject, '鲜活牛乳'),
+            data_get($goosItemsObject, '优果酪'),
+            data_get($goosItemsObject, '白小纯燕麦'),
+            data_get($goosItemsObject, '芝芝好莓'),
+            data_get($goosItemsObject, '芝芝好芒')
         ];
 
     }
